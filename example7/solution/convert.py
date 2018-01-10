83582c86216e1e43a3e1e2ba009c9838f1bb47fe
Al Johri <al.johri@gmail.com>
2018-01-10 04:39:22 -0500

    update example 7

---
+++ example7/solution/convert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import csv
+import datetime
+
+# see http://strftime.org/
+INPUT_DATE_FORMAT = "%m/%d/%Y"
+OUTPUT_DATE_FORMAT = "%-d-%b-%y"
+
+with open('input.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    # write csv header
+    writer.writerow(['date', 'close'])
+
+    for row in rows:
+        
+        # parse raw date
+        parsed_date = datetime.datetime.strptime(row['enddate'], INPUT_DATE_FORMAT)
+
+        # convert to output date format
+        output_date = parsed_date.strftime(OUTPUT_DATE_FORMAT)
+
+        # write row: date, approval
+        writer.writerow([output_date, row['approve']])
