06fe4afde0cd74861e0d589fd5f0a1bbbd29c4e5
Al Johri <al.johri@gmail.com>
2020-01-14 22:43:28 -0500

    Update convert.py

--- example7/solution/convert.py
+++ example7/solution/convert.py
@@ -7,23 +7,26 @@ import datetime
 INPUT_DATE_FORMAT = "%m/%d/%Y"
 OUTPUT_DATE_FORMAT = "%-d-%b-%y"
 
+# 1. load input.csv into a variable called `polls`
 with open('input.csv') as f:
     reader = csv.DictReader(f)
-    rows = list(reader)
+    polls = list(reader)
 
+# 2a. write a new file called output.csv
 with open('output.csv', 'w') as f:
     writer = csv.writer(f)
 
-    # write csv header
+    # 2b. write a row with two headers: "date" and "approve"
     writer.writerow(['date', 'close'])
 
-    for row in rows:
+    # 3. Loop through each row of `polls`
+    for row in polls:
         
-        # parse raw date
-        parsed_date = datetime.datetime.strptime(row['enddate'], INPUT_DATE_FORMAT)
+        # 4a. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
+        date = datetime.datetime.strptime(row['enddate'], INPUT_DATE_FORMAT)
 
-        # convert to output date format
-        output_date = parsed_date.strftime(OUTPUT_DATE_FORMAT)
+        # 4b. convert to output date format
+        new_date = date.strftime(OUTPUT_DATE_FORMAT)
 
-        # write row: date, approval
+        # # 5. write a new row of data with the transformed date and value for "approve" 
         writer.writerow([output_date, row['approve']])
